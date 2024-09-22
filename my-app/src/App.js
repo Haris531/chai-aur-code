c566104fca3d9b252253b1984e99c0a409c01bff
Haris531 <sharisahmed8811@gmail.com>
2024-09-22 18:29:48 +0500

    second commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import Chai from "./chai";
 import Pani from "./Pani";
+import Tea from "./Tea";
 
 function App() {
   return (
     <>
     <Chai/>
     <Pani/>
+    <Tea/>
     <h2>chai aur pani</h2>
     </>
   );
---
+++ my-app/src/Tea.js
@@ -0,0 +1,6 @@
+function Tea(){
+    return(
+        <h3>Tea aur chai</h3>
+    )
+}
+export default Tea
